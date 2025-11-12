d408f9d5945095b235cf9192568c32c62a479987
Nico Siirilä <nico.siirila@aalto.fi>
2025-11-12 17:20:10 +0200

    Camera Lerp

--- sidescroller/CameraFollower.gd
+++ sidescroller/CameraFollower.gd
@@ -1,6 +1,15 @@
 extends Camera2D
 
 @export var player: CharacterBody2D 
+@export_range(0.0, 1.0) var follow_speed = 0.1
+@export var follow_offset: float = 100
+
+var lastX: float = 0
+
 
 func _process(_delta: float) -> void:
-	self.global_position.x = player.global_position.x
+	var playerPos = player.global_position.x - follow_offset
+	
+	if lastX < playerPos:
+		self.global_position.x = lerpf(self.global_position.x, playerPos, follow_speed)
+		lastX = playerPos
